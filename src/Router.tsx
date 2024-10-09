0916d128eef92202741109726960395fe84abc49
Douglas <dhidekim@gmail.com>
2024-10-08 23:41:36 -0300

    Páginas e rotas - Layout de rotas

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,8 +5,10 @@ import { Home } from './pages/Home'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
