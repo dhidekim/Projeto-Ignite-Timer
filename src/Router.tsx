2c1233e85461b799006238b7cc1ab0ac030a076d
Douglas <dhidekim@gmail.com>
2024-10-08 23:37:44 -0300

    Páginas e rotas - React Router DOM

---
+++ src/Router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { History } from './pages/History'
+import { Home } from './pages/Home'
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/history" element={<History />} />
+    </Routes>
+  )
+}
