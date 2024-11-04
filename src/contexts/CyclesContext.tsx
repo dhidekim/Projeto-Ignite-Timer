b1760fbb166a30efdd669ed72ab209300bce7fcc
Douglas <dhidekim@gmail.com>
2024-11-03 21:58:52 -0300

    Contexto no React - Reset do formulário

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -57,7 +57,6 @@ export function CyclesContextProvider({
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-    // reset()
   }
   function interruptCurrentCycle() {
     setCycles((state) =>
